b6294ee8017d3935c33b0e5c18364c2255a8bd55
Huangkuanrong <b10802070@g.chu.edu.tw>
2023-02-21 16:41:17 +0800

    Add Finished Exercise 003.

--- LinqClassExercise003/Program.cs
+++ LinqClassExercise003/Program.cs
@@ -10,6 +10,21 @@ namespace LinqClassExercise003
     {
         static void Main(string[] args)
         {
+            int input = int.Parse(Console.ReadLine());
+            List<string> list = new List<string>();
+            for (int i = 1; i <= input; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    list.Add($"{input - i + 1}");
+                }
+                list.Add($"\n");
+            }
+            foreach (var i in list)
+            {
+                Console.Write(i);
+            }
+
         }
     }
 }
