3f5dc78e87a06270ab291030c97b3d0fcc0a9d0c
Huangkuanrong <b10802070@g.chu.edu.tw>
2023-02-21 11:37:26 +0800

    Add Finished Exercise 002.

--- LinqClassExercise002/Program.cs
+++ LinqClassExercise002/Program.cs
@@ -10,7 +10,29 @@ namespace LinqClassExercise002
     {
         static void Main(string[] args)
         {
+            List<string> input = Console.ReadLine().Split(',').ToList();
+            List<int> list = input.Select(x => int.Parse(x)).ToList();
+            List<int> oddNumber = list.Where(x => x%2!=0).OrderBy(y => y).ToList();
+            List<int> evenNumber = list.Where(x => x%2==0).OrderBy(y => y).ToList();
+            Display(oddNumber);
+            Display(evenNumber);
+        }
 
+        static void Display(List<int> output)
+        {
+            foreach (var item in output)
+            {
+                Console.Write(item);
+                if (output.IndexOf(item) < output.Count() - 1)
+                {
+                    Console.Write(",");
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+
+            }
         }
     }
 }
